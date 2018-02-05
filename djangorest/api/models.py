9521f24879a1e8baaf42f1845e30de67dbb007c4
vicervin <vicervin95@gmail.com>
2018-02-05 11:14:12 +0100

    Added owner to the ToDoList model

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class ToDoList(models.Model):
 
     name = models.CharField(max_length=255, blank=False, unique=True)
+    owner = models.ForeignKey('auth.User', related_name='todolists', on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_altered = models.DateTimeField(auto_now=True)
     done = models.BooleanField(default=False)
